52db904a3577dfe99c21b6a023fc2ddd028a28b3
Alob <afonso.baixo@ua.pt>
2025-04-11 09:29:45 +0100

    a

---
+++ Assignment2/Project/src/clientSide/entities/TPollster.java
@@ -0,0 +1,41 @@
+package Threads;
+import GUI.Gui;
+import Interfaces.ExitPoll.IExitPoll_Pollster;
+import Interfaces.GUI.IGUI_Common;
+
+public class TPollster implements Runnable {
+    private final IExitPoll_Pollster exitPoll;
+    private final IGUI_Common gui;
+
+    private TPollster(IExitPoll_Pollster exitPoll) {
+        this.exitPoll = exitPoll;
+        this.gui = Gui.getInstance();
+    }
+
+    public static TPollster getInstance(IExitPoll_Pollster exitPoll) {
+        return new TPollster(exitPoll);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Pollster running");
+        do {
+            try {
+                exitPoll.inquire();
+                
+                // Apply speed factor - slower speed = longer wait time
+                // Increase base wait time for pollster
+                float speedFactor = gui.getSimulationSpeed();
+                long waitTime = Math.round((Math.random() * 10 + 15) / speedFactor); // Increased from 5+5 to 10+15
+                Thread.sleep(waitTime);
+                
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while(exitPoll.isOpen());
+
+        exitPoll.printExitPollResults();
+
+        System.out.println("Pollster terminated");
+    }
+}
