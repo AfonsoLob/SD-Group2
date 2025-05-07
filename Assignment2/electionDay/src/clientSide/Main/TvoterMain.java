0c96c241b285c0de4efc059fd2b4b0616204d6b8
Alob <afonso.baixo@ua.pt>
2025-05-07 18:27:57 +0100

    reduced parameter verifciation (TODO: fix getInstance parameters)

--- Assignment2/electionDay/src/clientSide/Main/TvoterMain.java
+++ Assignment2/electionDay/src/clientSide/Main/TvoterMain.java
@@ -4,34 +4,25 @@ import clientSide.entities.TVoter;
 import clientSide.stubs.MPollingStationStub;
 import clientSide.stubs.MExitPollStub;
 
+import serverSide.main.SimulPar;
+
 public class TvoterMain {
     public static void main(String[] args) {
-        // Check if number of voters is provided
-        if (args.length != 1) {
-            System.out.println("Usage: java TvoterMain <number_of_voters>");
-            System.exit(1);
-        }
-
-        // Parse number of voters
-        int numVoters = Integer.parseInt(args[0]);
-        if (numVoters < 3 || numVoters > 10) {
-            System.out.println("Number of voters must be between 3 and 10");
-            System.exit(1);
-        }
-
+        SimulPar.validateParameters();
+        
         // Stubs
         MPollingStationStub pollingStation = new MPollingStationStub();
         MExitPollStub exitPoll = new MExitPollStub();
-
+        
         // Create and start voter threads
-        Thread[] voters = new Thread[numVoters];
-        for (int i = 0; i < numVoters; i++) {
+        Thread[] voters = new Thread[SimulPar.N];
+        for (int i = 0; i < SimulPar.N; i++) {
             voters[i] = new Thread(TVoter.getInstance(i, pollingStation, exitPoll));
             voters[i].start();
         }
 
         // Wait for all voters to finish
-        for (int i = 0; i < numVoters; i++) {
+        for (int i = 0; i < SimulPar.N; i++) {
             try {
                 voters[i].join();
             } catch (InterruptedException e) {
