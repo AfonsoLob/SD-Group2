4c755590a938d42ec616aafe2d8dd7d791323128
Alob <afonso.baixo@ua.pt>
2025-05-08 18:53:03 +0100

    pollster main

--- Assignment2/electionDay/src/clientSide/main/TpollsterMain.java
+++ Assignment2/electionDay/src/clientSide/main/TpollsterMain.java
@@ -1,5 +1,46 @@
 package clientSide.main;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import clientSide.entities.TPollster;
+import clientSide.stubs.MExitPollStub;
+import clientSide.interfaces.ExitPoll.IExitPoll_Pollster;
+
+import serverSide.main.SimulPar;
+
 public class TpollsterMain {
-    
+    public static void main(String[] args) {
+        SimulPar.validateParameters();
+
+        final String CONFIG_FILE = "config.properties";               // Path to the configuration 
+        int ServerExitPollPortNumber = -1;                           // port number for listening to service requests
+        String hostExitPollName = "exitPoll";                        // name of the platform where is located the exit poll server
+
+        // Load default values from properties file
+        Properties props = new Properties();
+        try (FileInputStream fis = new FileInputStream(CONFIG_FILE)) {
+            props.load(fis);
+            hostExitPollName = props.getProperty("hostExitPollName");
+            ServerExitPollPortNumber = Integer.parseInt(props.getProperty("ServerExitPollPortNumber"));
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("Error reading configuration file!");
+            System.exit(1);
+        }
+        
+        // Stub
+        IExitPoll_Pollster exitPoll = new MExitPollStub(hostExitPollName, ServerExitPollPortNumber);
+        
+        // Create and start pollster thread
+        Thread pollster = new Thread(TPollster.getInstance(exitPoll));
+        pollster.start();
+
+        // Wait for pollster to finish
+        try {
+            pollster.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
